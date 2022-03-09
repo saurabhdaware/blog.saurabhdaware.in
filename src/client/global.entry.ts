31b2554201384fce9f9761051872b572285f8192
saurabhdaware <saurabhdaware99@gmail.com>
2022-03-10 00:17:36 +0530

    feat: delete analytics, move to count API

--- src/client/global.entry.ts
+++ src/client/global.entry.ts
@@ -1,4 +1,9 @@
 import '@fontsource/inter';
+import { countPageVisit } from './counter';
 import './global.inlined.css';
 
 document.querySelector<HTMLSpanElement>('footer span.footer-year').innerText = String(new Date().getFullYear());
+
+window.addEventListener('load', () => {
+  countPageVisit();
+})
